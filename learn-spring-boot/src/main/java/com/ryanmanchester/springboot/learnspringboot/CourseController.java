8e6ee69babe63aa0db47260d981db34dcd5b081b
Ryan Manchester <ryanm17@gmail.com>
2023-11-20 21:39:34 -0500

    implements basic spring boot api

---
+++ learn-spring-boot/src/main/java/com/ryanmanchester/springboot/learnspringboot/CourseController.java
@@ -0,0 +1,20 @@
+package com.ryanmanchester.springboot.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses() {
+		return Arrays.asList(
+				new Course(1, "Learn AWS", "Raj"),
+				new Course(2, "Learn DevOps", "Ben")
+				);
+	}
+
+}
